7f825f3b1d157d1152c657f0e3183946312bf751
Fabiano da Silva Oliveira <fabianojuara@hotmail.com>
2022-05-26 10:35:30 -0400

    Adicionado Exercicios da Lista1

--- Lista1.cs
+++ Lista1.cs
@@ -1,33 +1,77 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace exercicos
 {
-    internal class Lista1
+     class Lista1
     {
-        void exe1()
+        public void exe1()
         {
             int x,y,b;
             x = 15;
             y = 20;
+            Console.WriteLine("Lista 1 - Exercicio 1");
             Console.WriteLine(x);
             b = 3;
-            Console.WriteLine($"{x} {y}");
+            Console.WriteLine($"{x},{y}");
         }
-        void exe2()
+        public void exe2()
         {
             int a, b, c;
             a = 15;
             b = 3;
             c = a * b;
+            Console.WriteLine("Lista 1 - Exercicio 2");
             Console.WriteLine(c);
             b = 10;
             c = a - b;
-            Console.WriteLine($"{a} {b} {c}");
+            Console.WriteLine($"{a},{b},{c}");
 
+        }
+        public void exe3()
+        {
+            int x, y, z;
+            x = 10;
+            y = 12;
+            z = x;
+            y = z;
+            x = y;
+            Console.WriteLine("Lista 1 - Exercicio 3");
+            Console.WriteLine($"{x},{y},{z}");
+        }
+        public void exe4()
+        {
+            int a, b;
+            a = 10;
+            b = a + 2;
+            a = b + 1;
+            b = a + 1;
+            Console.WriteLine("Lista 1 - Exercicio 4");
+            Console.WriteLine(a);
+            a = b + 1;
+            Console.WriteLine($"{a},{b}");
+        }
+        public void exe5()
+        {
+            int x, y, z;
+            x = 12;
+            y = x;
+            z = x + y;
+            x = 20;
+            y = 10;
+            Console.WriteLine("Lista 1 - Exercicio 5");
+            Console.WriteLine($"{x},{y},{z}");
+        }
+        public void exe6()
+        {
+            int a, b, c;
+            a = 2;
+            b = 3;
+            a = a + b;
+            c = a - b;
+            Console.WriteLine("Lista 1 - Exercicio 6");
+            Console.WriteLine(c);
+            b = 5;
+            Console.WriteLine($"{a},{b},{c}");
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,11 +6,32 @@ using System.Threading.Tasks;
 
 namespace exercicos
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            
+            int opc = 0;
+            Lista1 lista1 = new Lista1();
+            switch(opc){
+                case 1:
+                    lista1.exe1();
+                    break;
+                case 2:
+                    lista1.exe2();
+                    break;
+                case 3:
+                    lista1.exe3();
+                    break;
+                case 4:
+                    lista1.exe4();
+                    break;
+                case 5:
+                    lista1.exe5();
+                    break;
+                case 6:
+                    lista1.exe6();
+                    break;
+            }            
         }
     }
 }
