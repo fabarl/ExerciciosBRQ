ace6d837324af6bb672baee2ca4107a0258efcb0
Fabiano da Silva Oliveira <fabianojuara@hotmail.com>
2022-05-26 09:45:19 -0400

    Adicionar arquivos de projeto.

---
+++ Lista1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicos
+{
+    internal class Lista1
+    {
+        void exe1()
+        {
+            int x,y,b;
+            x = 15;
+            y = 20;
+            Console.WriteLine(x);
+            b = 3;
+            Console.WriteLine($"{x} {y}");
+        }
+        void exe2()
+        {
+            int a, b, c;
+            a = 15;
+            b = 3;
+            c = a * b;
+            Console.WriteLine(c);
+            b = 10;
+            c = a - b;
+            Console.WriteLine($"{a} {b} {c}");
+
+        }
+    }
+}
